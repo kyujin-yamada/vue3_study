c1b60b6b08aaafe1f731d34dcb48523741e0ba1a
kyujin-yamada <39543664+kyujin-yamada@users.noreply.github.com>
2021-10-18 14:51:15 +0900

    Add files via upload

---
+++ src/views/ExperienceShow.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <img :src="`/images/${experience.image}`"  :alt="experience.name" />
+        <p>{{ experience.description }}</p>
+    </section>
+</template>
+<script>
+import { defineComponent } from 'vue'
+import sourceData  from '../data.json'
+export default defineComponent({
+    props:{
+        id: {type: Number, required: true,},
+        experienceSlug: {type: String,  required: true,}
+    },
+    computed:{
+        destination(){
+            return sourceData.destinations.find(
+                destination => destination.id === this.id
+            );
+        },
+        experience(){
+            return this.destination.experiences.find(
+                experience => experience.slug === this.experienceSlug
+            );
+        }
+    }
+})
+</script>
